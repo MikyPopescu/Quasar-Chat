21b2f170df58220826c590462b020c3098e56bfb
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-04-19 22:11:08 +0300

    design Users & Chat

---
+++ src/pages/PageAuth.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex">
+    <h5>Auth Page</h5>
+  </q-page>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/PageUsers.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item v-for="user in users" :key="user.id" to="/chat" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">{{ user.name.charAt(0) }}</q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge
+            :color="user.online ? 'green-6' : 'grey-4'"
+          >{{user.online ? 'Online' : 'Offline'}}</q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'Miky',
+          online: true
+        },
+        {
+          id: 2,
+          name: 'Gigel',
+          online: false
+        },
+        {
+          id: 3,
+          name: 'Dorel',
+          online: true
+        }
+      ]
+    }
+  }
+}
+</script>
